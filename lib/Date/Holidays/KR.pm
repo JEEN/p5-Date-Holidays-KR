3d04dceed574e1363c5f6301305fdef7f40c492b
Jeen Lee <jeen@perl.kr>
2012-02-07 18:12:57 +0900

    initial commit

---
+++ lib/Date/Holidays/KR.pm
@@ -0,0 +1,32 @@
+package Date::Holidays::KR;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Date::Holidays::KR -
+
+=head1 SYNOPSIS
+
+  use Date::Holidays::KR;
+
+=head1 DESCRIPTION
+
+Date::Holidays::KR is
+
+=head1 AUTHOR
+
+Jeen Lee E<lt>aiatejin {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
